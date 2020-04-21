661e6fc8f8df693b15196d16eb2d9dff469a8448
Zach <zacharyrrosenberg@gmail.com>
2020-04-21 19:59:22 -0400

    updates to sample_test for more usability

--- sample_test.py
+++ sample_test.py
@@ -8,6 +8,7 @@
 #       to find the correct place to provide that key..
 
 import argparse
+import json
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -21,16 +22,19 @@ DEVELOPER_KEY = 'AIzaSyDJ7jIvgvXxY4Ox9kLONyhEl3O6s9-mM_4'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
-def youtube_search(options):
-    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    developerKey=DEVELOPER_KEY)
+
+def youtube_search(query, max_results):
+    youtube = build(
+        YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+        developerKey=DEVELOPER_KEY
+    )
 
     # Call the search.list method to retrieve results matching the specified
     # query term.
     search_response = youtube.search().list(
-    q=options.q,
-    part='id,snippet',
-    maxResults=options.max_results
+        q=query,
+        part='id,snippet',
+        maxResults=max_results
     ).execute()
 
     videos = []
@@ -41,28 +45,40 @@ def youtube_search(options):
     # matching videos, channels, and playlists.
     for search_result in search_response.get('items', []):
         if search_result['id']['kind'] == 'youtube#video':
-            videos.append('%s (%s)' % (search_result['snippet']['title'],
-                                        search_result['id']['videoId']))
+            videos.append(
+                '%s (%s)' % (
+                    search_result['snippet']['title'],
+                    search_result['id']['videoId'])
+            )
         elif search_result['id']['kind'] == 'youtube#channel':
-            channels.append('%s (%s)' % (search_result['snippet']['title'],
-                                        search_result['id']['channelId']))
+            channels.append(
+                '%s (%s)' % (
+                    search_result['snippet']['title'],
+                    search_result['id']['channelId']
+                )
+            )
         elif search_result['id']['kind'] == 'youtube#playlist':
-            playlists.append('%s (%s)' % (search_result['snippet']['title'],
-                                    search_result['id']['playlistId']))
+            playlists.append(
+                '%s (%s)' % (
+                    search_result['snippet']['title'],
+                    search_result['id']['playlistId']
+                )
+            )
 
     print('Videos:\n', '\n'.join(videos), '\n')
     print('Channels:\n', '\n'.join(channels), '\n')
     print('Playlists:\n', '\n'.join(playlists), '\n')
 
-    from pprint import pprint
     vids = search_response.get('items', [])
-
-    import json
-    with open('video_data.json', 'w') as fname:
-        json.dump(vids, fname, indent=4)
+    # return the search results from this fuction
+    return vids
 
 
-
+def json_dump(json_data, filename):
+    # dump the json_json data to a file with filename
+    json_fname = '{}.json'.format(filename)
+    with open(json_fname, 'w') as fname:
+        json.dump(json_data, fname, indent=4)
 
 
 if __name__ == '__main__':
@@ -71,8 +87,16 @@ if __name__ == '__main__':
     parser.add_argument('--max-results', help='Max results', default=25)
     args = parser.parse_args()
 
+    # get the query and max_results inputs
+    query = args.q
+    max_results = args.max_results
+
     try:
-        youtube_search(args)
+        # assign search results to variable
+        search_results = youtube_search(query, max_results)
     except HttpError as e:
-        print('An HTTP error {} occurred:\n{}'.format(e.resp.status, e.content))
-    
+        print(
+            'An HTTP error {} occurred:\n{}'.format(e.resp.status, e.content)
+        )
+    json_dump(search_results, 'video_data')
+
